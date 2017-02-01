cda35c0e4865f182024129c55a3ce2694f68097c
ZhakDenys <napster.sean.parker@gmail.com>
2017-02-01 18:58:49 +0200

    Create Application.java

---
+++ src/Application.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Application {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                GUI gui = new GUI();
+            }
+        });
+    }
+}
